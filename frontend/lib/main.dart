1c37037ec107358c0dc947a0eda5e868623296fa
Humayra Mahboob <humayra.mahboob01@myhunter.cuny.edu>
2025-10-28 16:35:04 -0400

    fixed nav bar and added event modification in calendar

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/launch_screen.dart';
+import 'screens/dashboard_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LaunchScreen(),
+      home: const DashboardScreen(),
     );
   }
 }
