40d1dc617700d06965d4e951e1c56d5636954a47
Humayra Mahboob <humayra.mahboob01@myhunter.cuny.edu>
2025-10-30 22:37:17 -0400

    cleaned up files

--- frontend/lib/screens/onboarding_screen.dart
+++ frontend/lib/screens/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/dashboard_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:frontend/screens/dashboard_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
