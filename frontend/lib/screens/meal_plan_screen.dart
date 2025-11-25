4301aa696d2c8c5b6c106c1b27b19de41546bd7a
humayramahboob <humayra_mahbhoob01@myhunter.cuny.edu>
2025-11-25 03:24:53 -0500

    added swipe to delete function to meal page

--- frontend/lib/screens/meal_plan_screen.dart
+++ frontend/lib/screens/meal_plan_screen.dart
@@ -330,8 +330,7 @@ class _MealPlanScreenState extends State<MealPlanScreen> {
                                     borderRadius: BorderRadius.circular(14),
                                     boxShadow: [
                                       BoxShadow(
-                                        color:
-                                            Colors.black.withOpacity(0.12),
+                                        color: Colors.black.withValues(alpha: 0.12),
                                         blurRadius: 10,
                                         offset: const Offset(0, 4),
                                       )
